1109c30c11d079fe22a96a1e863cc4c92bdfec0e
kuzurgalievra <kuzurgalievra@teletribe.ru>
2026-07-04 21:35:19 +0400

    add new endpoints

---
+++ backend/app/Http/Controllers/TrainingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TrainingController extends Controller
+{
+    public function newWord() {
+        return response()->json();
+    }
+
+    public function repeatWord() {
+        return response()->json();
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DictionaryController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProgressController;
+use App\Http\Controllers\TrainingController;
 use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,5 +17,12 @@ Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
 Route::group(['middleware' => AuthMiddleware::class], function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/languages', [LanguageController::class, 'all'])->name('languages');
+
+    Route::get('/profile', [ProfileController::class, 'profile'])->name('profile');
+    Route::get('/training', [TrainingController::class, 'newWord'])->name('new-word');
+    Route::patch('/training', [TrainingController::class, 'repeatWord'])->name('repeat-word');
+
+    Route::get('/dictionary/{baseTrainingId}/language/{targetLanguageId}', [DictionaryController::class, 'translate'])->name('translate');
+    Route::get('/progress/{status}', [ProgressController::class, 'progress'])->name('progress');
 });
 
