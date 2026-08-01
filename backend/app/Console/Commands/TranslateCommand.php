15793c1c81cbdf91bc37da735095d6d18eb60332
kuzurgalievra <kuzurgalievra@teletribe.ru>
2026-08-01 20:34:23 +0400

    add new datasets

--- backend/app/Console/Commands/TranslateCommand.php
+++ backend/app/Console/Commands/TranslateCommand.php
@@ -16,13 +16,48 @@ class TranslateCommand extends Command
      */
     public function handle()
     {
-        $sourceLanguage = 'en';
-        $targetLanguage = 'zh';
-        $word = 'car';
-        $url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=$sourceLanguage&tl=$targetLanguage&dt=t&q=$word";
-        $response = Http::get($url);
+        $path = base_path('data/words/');
+        $rawDatasets = [
+            'pt', 'ar', 'ko'
+        ];
 
-        $translation = $response->json()[0][0][0];
-        echo $response;
+        foreach ($rawDatasets as $rawDataset) {
+            $file = fopen($path . 'en/fr.jsonl', "r");
+            $index = 0;
+            $fopen = fopen($path . 'en/'. $this->transform($rawDataset) . '.jsonl', "w");
+            while (($line = fgets($file)) !== false) {
+                $word = json_decode($line);
+                $sourceLanguage = 'en';
+                $targetLanguage = $rawDataset;
+                $level = $word->level;
+                $word = $word->en;
+                $url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=$sourceLanguage&tl=$targetLanguage&dt=t&q=$word";
+                $response = Http::get($url);
+                $translation = $response->json()[0][0][0];
+                $index++;
+                fwrite($fopen, json_encode(
+                    [
+                        'en' => $word,
+                        $this->transform($rawDataset) => strtolower($translation),
+                        'level' => $level,
+                    ], JSON_UNESCAPED_UNICODE
+                ) . PHP_EOL);
+                echo $index . '   ----   ' . $word . '  ---  '. $translation . PHP_EOL;
+            }
+            fclose($file);
+            fclose($fopen);
+        }
+    }
+
+    public function transform($language)
+    {
+        switch ($language) {
+            case 'ko':
+                return 'kr';
+            case 'ar':
+                return 'sa';
+            default:
+                return $language;
+        }
     }
 }
